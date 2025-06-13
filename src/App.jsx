5f79e8f30e2dc7abe0f0ef16a11e84a438d9ac5a
Shashank463 <kashyapshashank463@gmail.com>
2025-06-13 16:36:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Images from './components/images'
+import Navbar from './components/navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Images />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/images.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import pixabayContext from '../context/pixabayContext'
+
+const Images = () => {
+    const data = useContext(pixabayContext);
+    return (
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', marginTop: ''}}>
+            {data.Images.map((Image) => (<div key={Image.id} style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', height: 'auto', width: '30%', gap: '5px', borderRadius: '25px'}}>
+                <div className="item" >
+                    <img src={Image.largeImageURL} alt="." style={{marginTop: '10px', borderRadius: "25px", border: '2px solid yellow', height: '75%', width: '100%'}}/>
+                </div>
+            </div>))}
+        </div>
+    )
+}
+
+export default Images
+
+
