0e0653af7bf184582260f89ce06654fd223a0471
Shashank463 <kashyapshashank463@gmail.com>
2025-06-13 17:47:58 +0530

    Fix: corrected pixabayContext usage in images.jsx

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import PixabayContext from '../context/pixabayContext'
 
 const Images = () => {
-    const data = useContext(pixabayContext);
+    const data = useContext(PixabayContext);
     return (
         <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', marginTop: ''}}>
             {data.Images.map((Image) => (<div key={Image.id} style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', height: 'auto', width: '30%', gap: '5px', borderRadius: '25px'}}>
